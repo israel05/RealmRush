4963405bab7a7f2f826c5317e33fc3dce9efdf31
israeldelamo <Angramanyu_05>
2020-05-13 19:14:02 +0200

    Nuevo Cubo Prefab

---
+++ RealmRUSH/Assets/EditorSnap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorSnap : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Editor causa Awake"); 
+    }
+    private void Update()
+    {
+        Debug.Log("Editor casa Update");
+    }
+}
