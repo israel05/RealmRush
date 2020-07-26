b357fbd6a99da428a7934fe433d582f78d573df3
GRANDE-I7\idela <i.delamo@gmail.com>
2020-07-26 19:23:01 +0200

    Script en el editor

--- RealmRUSH/Assets/EditorSnap.cs
+++ RealmRUSH/Assets/EditorSnap.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[ExecuteInEditMode]
 public class EditorSnap : MonoBehaviour
 {
-    private void Awake()
-    {
-        Debug.Log("Editor causa Awake"); 
-    }
-    private void Update()
+       private void Update()
     {
         Debug.Log("Editor casa Update");
+        Vector3 snapPosition;
+        snapPosition.x = Mathf.RoundToInt( transform.position.x / 10f) *10f;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / 10f) * 10f;
+        transform.position = new Vector3(snapPosition.x, 0.0f, snapPosition.z);
     }
 }
