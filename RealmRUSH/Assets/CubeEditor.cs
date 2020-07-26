75b269bbf8acbbb33d74ca71287107bd4ab8d89b
GRANDE-I7\idela <i.delamo@gmail.com>
2020-07-26 21:39:20 +0200

    Patrulla

---
+++ RealmRUSH/Assets/CubeEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[SelectionBase]
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
+{
+
+
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMeshTop;
+   // TextMesh textMeshIzq;
+
+
+    private void Start()
+    {
+       
+    }
+
+    private void Update()
+    {
+        
+        Vector3 snapPosition;
+    
+        snapPosition.x = Mathf.RoundToInt( transform.position.x / gridSize) * gridSize;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPosition.x, 0.0f, snapPosition.z);
+
+        textMeshTop = GetComponentInChildren<TextMesh>();
+       string labelText = (snapPosition.x / gridSize).ToString() + "," + (snapPosition.z / gridSize).ToString();
+
+        textMeshTop.text = labelText;
+        gameObject.name = labelText;
+
+
+    }
+}
---
+++ RealmRUSH/Assets/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] List<Waypoint> path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Starting Patrol...");
+        foreach (Waypoint waypoint in path)
+        {
+            print("Visiting block: " + waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending Patrol...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
