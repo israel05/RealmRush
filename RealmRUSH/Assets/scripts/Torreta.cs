e389a62c1a7c030163d7a4eab63dbd60f08eb016
GRANDE-I7\idela <i.delamo@gmail.com>
2020-07-30 23:10:10 +0200

    Cesa el fuego si el enemigo se destruye

--- RealmRUSH/Assets/scripts/Torreta.cs
+++ RealmRUSH/Assets/scripts/Torreta.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,39 @@ public class Torreta : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
-
+    [SerializeField] float range = 10f;
+    [SerializeField] ParticleSystem projectileParticle;
     void Update()
     {
-        objectToPan.LookAt(targetEnemy);
+        if (targetEnemy)
+        {
+            objectToPan.LookAt(targetEnemy);
+            FireAtEnemy();
+
+        } else
+        {
+            Shoot(false);
+        }
+
+
     }
 
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.transform.position, gameObject.transform.position);
+     //   print("distancia hasta el enemigo :" + distanceToEnemy);
+        if (distanceToEnemy <= range)
+        {
+            Shoot(true);
+        } else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
+    }
 }
