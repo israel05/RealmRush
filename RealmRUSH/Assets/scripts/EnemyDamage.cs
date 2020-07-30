65cb279adf98178e9dec53d4a8d774df9f13f75b
GRANDE-I7\idela <i.delamo@gmail.com>
2020-07-30 14:30:50 +0200

    Las torretas lanzan proyectiles que alcanzan al objetivo y notifica impacto

---
+++ RealmRUSH/Assets/scripts/EnemyDamage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print("¡Tocado!");
+    }
+}
