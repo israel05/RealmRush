39191154d19301a911114c4511765ed3344b8caa
GRANDE-I7\idela <i.delamo@gmail.com>
2020-07-31 00:10:36 +0200

    Creando enemigos sin fin

---
+++ RealmRUSH/Assets/scripts/enemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMovement enemyPrefab; //al poner enemyMovement que es un script
+    // propio de enemigo, nos eviatamos poner cualquier gameobject que es genérico
+    
+    void Start()
+    {
+        //lanza la corutina
+        StartCoroutine(RepeatedlySpawnEnemies());
+    }
+
+    private IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true)
+        {
+            print("Creando enemigo");
+            yield return new WaitForSeconds(secondsBetweenSpawns); 
+        }               
+    }
+
+
+
+}
