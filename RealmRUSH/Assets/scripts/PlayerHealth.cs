68c4eea625027cfc15f22353133406285c0ae3d9
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-01 16:11:42 +0200

    Puntos de vida del edificio creados

---
+++ RealmRUSH/Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    [SerializeField] int health = 10;
+    [SerializeField] int healthDecrease = 1;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        health = health - healthDecrease;
+
+        if (health < 1)
+        {
+            Destroy(gameObject);
+        }        
+    }
+}
