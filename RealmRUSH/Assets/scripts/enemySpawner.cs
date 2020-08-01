884db173ebbdca8b4b1e51dc0306be6be0307d9f
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-01 23:39:30 +0200

    Contador de numero de enemigos en pantalla

--- RealmRUSH/Assets/scripts/enemySpawner.cs
+++ RealmRUSH/Assets/scripts/enemySpawner.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class enemySpawner : MonoBehaviour
 {
@@ -8,6 +10,8 @@ public class enemySpawner : MonoBehaviour
     [SerializeField] float secondsBetweenSpawns = 2f;
     [SerializeField] EnemyMovement enemyPrefab; //al poner enemyMovement que es un script
     [SerializeField] Transform enemyParentTransfor;
+    [SerializeField] Text enemySpawnCounter;
+    int contadorEnemySpawnCounter = 0;
 
     // propio de enemigo, nos eviatamos poner cualquier gameobject que es genérico
 
@@ -15,15 +19,25 @@ public class enemySpawner : MonoBehaviour
     {
         //lanza la corutina
         StartCoroutine(RepeatedlySpawnEnemies());
+    
     }
+
+
 
     private IEnumerator RepeatedlySpawnEnemies()
     {
         while (true)
-        {       
+        {
             var newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity); //spawn el enemigo, en la posicion del enemigo inicial, con rotacion ninguna
+            IncrementoContadorEnemigosEnPantalla();
             newEnemy.transform.parent = enemyParentTransfor;
-            yield return new WaitForSeconds(secondsBetweenSpawns); 
-        }               
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+
+    private void IncrementoContadorEnemigosEnPantalla()
+    {
+        contadorEnemySpawnCounter++;
+        enemySpawnCounter.text = contadorEnemySpawnCounter.ToString();
     }
 }
