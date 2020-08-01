fad21364bffcd8c672c49befd2a1e32023a64588
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-01 23:33:12 +0200

    Texto UI VIda restante

--- RealmRUSH/Assets/scripts/PlayerHealth.cs
+++ RealmRUSH/Assets/scripts/PlayerHealth.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerHealth : MonoBehaviour
 {
 
     [SerializeField] int health = 10;
     [SerializeField] int healthDecrease = 1;
+    [SerializeField] Text healthText ;
     // Start is called before the first frame update
+
+
+    private void Start()
+    {
+        healthText.text = health.ToString();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        health = health - healthDecrease;
-
-        if (health < 1)
-        {
-            Destroy(gameObject);
-        }        
+        health -= healthDecrease;
+        healthText.text = health.ToString();
     }
 }
